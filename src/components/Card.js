05beea4b34040630f3d07bf533f38229db9747b7
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-08-24 18:11:07 -0600

    changing styling of hero and products

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import Star1 from './images/Star1.png';
 
-export default function Card({ stats, price, description, title, coverImg }) {
+export default function Card({ stats, price, description, title, coverImg, openSpots }) {
   const cardStyle = {
     display: 'flex',
     float: 'left', // Float the Card component to the left
@@ -8,24 +8,29 @@ export default function Card({ stats, price, description, title, coverImg }) {
     /* You can add additional flex properties here if needed */
   };
 
-  return (
-    <div style={cardStyle} className="card-container">
-      <div>
-        <img
-          src={coverImg}
-          alt={coverImg}
-          className="card--image"
-        />
+  let BadgeStyle
+   if ( openSpots === 0){
+         BadgeStyle = 'Sold Out'
+   } else{
+        BadgeStyle = 'Online'
+   }
 
-        <div className="card--stats">
-          <img src={Star1} alt="Star Icon" />
-          <span>{stats.rating}</span>
-          <span className='gray'>({stats.reviewCount})</span>
+  return (
+        <div className='Cards--Section'> 
+             {BadgeStyle && <div className="BadgeStyle"> {BadgeStyle}</div>}
+             <img
+                src={coverImg}
+                alt={coverImg}
+                className="card--image"
+             />
+            <div className="card--stats">
+                <img src={Star1} alt="Star Icon" />
+                <span>{stats.rating}</span>
+                <span className='gray'>({stats.reviewCount})</span>
+            </div>
+            <p>{title}</p>
+            <p><span className="bold"> from ${price}</span> / person</p>
         </div>
-        <p>{title}</p>
-        <p>{description}</p>
-        <p><span className="bold"> from ${price}</span> / person</p>
-      </div>
-    </div>
+       
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header(){ 
+    return (
+    <Header className="header"> 
+        <div>
+            <h2>Header Component</h2>
+        </div>
+        <h4>React Course - Project 3</h4>         
+    </Header>
+    )
+} 
